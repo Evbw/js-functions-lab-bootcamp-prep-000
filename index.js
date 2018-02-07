98f4db8d20a6eabc050e702bce6d46d6b8019b30
Everett <everett.bowline@gmail.com>
2018-02-07 12:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return 'Happy holidays, ${name}'
 }
 
-happyHolidaysTo( ${you})
+happyHolidaysTo(${you})
 
 function happyHolidayTo(holiday) {
   return 'Happy ${holiday}, ${name}'
